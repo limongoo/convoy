798685628c1f0796d0d168d6adc60f98cbc6f9f9
ivan limongan <ivan.limongan@gmail.com>
2018-10-04 21:19:43 -0700

    image css updated

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -3,17 +3,19 @@ import './image.css';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 const Image = props =>
-  <ScrollAnimation animateIn='fadeIn'>
-    <li className="image-li">
-      <a href={props.link} target="_blank" rel="author noopener noreferrer">
+  
+  <li className="image-li">
+    <a href={props.link} target="_blank" rel="author noopener noreferrer">
+      <ScrollAnimation animateIn='fadeIn'>
         <img src={props.url} alt={props.name} />
-      </a>
-      <p className="image-description">
+      </ScrollAnimation>
+    </a>
+    <p className="image-description">
 			Photo: 
-        <a href={props.user} target="_blank" rel="author noopener noreferrer"> {props.name}</a> — 
-        <a href={props.link} target="_blank" rel="author noopener noreferrer"> Unsplash</a>
-      </p>
-    </li>
-  </ScrollAnimation>;
+      <a href={props.user} target="_blank" rel="author noopener noreferrer"> {props.name}</a> — 
+      <a href={props.link} target="_blank" rel="author noopener noreferrer"> Unsplash</a>
+    </p>
+  </li>;
+  
 
 export default Image;
